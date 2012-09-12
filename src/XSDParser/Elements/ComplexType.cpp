4a1af5b1987807252dd07566cd50c9daf6b8f597
ardy <afalls@qvxlabs.com>
2012-09-11 23:12:31 -0700

    Fixed a bug with complextype. It now properly throws an error if an attribute and a content type is defined. Also enabled complex type to now properly have attribute groups as child elements (this was not there before and was a bug).

--- src/XSDParser/Elements/ComplexType.cpp
+++ src/XSDParser/Elements/ComplexType.cpp
@@ -32,8 +32,10 @@
 #include "./src/XSDParser/Elements/Node.hpp"
 #include "./src/XSDParser/Elements/Sequence.hpp"
 #include "./src/XSDParser/Elements/Attribute.hpp"
+#include "./src/XSDParser/Elements/AttributeGroup.hpp"
 #include "./src/XSDParser/Elements/Choice.hpp"
 #include "./src/XSDParser/Elements/Schema.hpp"
+#include "./src/XSDParser/Elements/SimpleContent.hpp"
 #include "./src/XSDParser/Elements/ComplexContent.hpp"
 #include "./src/XSDParser/Elements/Group.hpp"
 #include "./src/XSDParser/Elements/Annotation.hpp"
@@ -55,14 +57,28 @@ ComplexType::ParseChildren(BaseProcessor& rProcessor) const throw(XMLException) 
 	/* process children */
 	std::auto_ptr<Node> pNode(Node::FirstChild());
 	if (NULL != pNode.get()) {
+		enum { CMODEL_UNKNOWN, CMODEL_TRUE, CMODEL_FALSE };
+		int contentModel = CMODEL_UNKNOWN;
 		do {
-			if (XSD_ISELEMENT(pNode.get(), Group) ||
-				XSD_ISELEMENT(pNode.get(), Choice) ||
-				XSD_ISELEMENT(pNode.get(), Sequence) ||
-				XSD_ISELEMENT(pNode.get(), Annotation) ||
-				XSD_ISELEMENT(pNode.get(), Attribute) ||
-				XSD_ISELEMENT(pNode.get(), All) ||
-				XSD_ISELEMENT(pNode.get(), ComplexContent)) {
+			if (XSD_ISELEMENT(pNode.get(), ComplexContent) ||
+				XSD_ISELEMENT(pNode.get(), SimpleContent)) {
+				if (CMODEL_FALSE != contentModel) {
+					contentModel = CMODEL_TRUE;
+					pNode->ParseElement(rProcessor);
+				} else
+					throw XMLException(pNode->GetXMLElm(), XMLException::InvallidChildXMLElement);
+			} else if ( XSD_ISELEMENT(pNode.get(), Group) ||
+						XSD_ISELEMENT(pNode.get(), All) ||
+						XSD_ISELEMENT(pNode.get(), Choice) ||
+						XSD_ISELEMENT(pNode.get(), Sequence) ||
+						XSD_ISELEMENT(pNode.get(), Attribute) ||
+						XSD_ISELEMENT(pNode.get(), AttributeGroup)) {
+				if (CMODEL_TRUE != contentModel) {
+					contentModel = CMODEL_FALSE;
+					pNode->ParseElement(rProcessor);
+				} else
+					throw XMLException(pNode->GetXMLElm(), XMLException::InvallidChildXMLElement);
+			} else if (XSD_ISELEMENT(pNode.get(), Annotation)) {
 				pNode->ParseElement(rProcessor);
 			} else
 				throw XMLException(pNode->GetXMLElm(), XMLException::InvallidChildXMLElement);
